cfa53f67370b164848d8ed388c3472ccfe3cd7fb
Vence Lin <vence.lin@regtics.com>
2022-03-16 10:26:36 +0800

    init commit

---
+++ bearer_token.go
@@ -0,0 +1,57 @@
+package bearertoken
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+type Handler func(*gin.Context)
+
+type Options struct {
+	OnAuthorizationHeaderMissing Handler
+	OnAuthorizationHeaderInvalid Handler
+	OnTokenInvalid               Handler
+}
+
+func Middleware(token string, opt ...Options) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		// Authorization header is missing in HTTP request
+		if authHeader == "" {
+			if len(opt) > 0 && opt[0].OnAuthorizationHeaderMissing != nil {
+				opt[0].OnAuthorizationHeaderMissing(c)
+			} else {
+				c.AbortWithStatus(http.StatusBadRequest)
+			}
+			return
+		}
+
+		authTokens := strings.Split(authHeader, " ")
+
+		// The value of authorization header is invalid
+		// It should start with "Bearer"
+		if authTokens[0] != "Bearer" {
+			if len(opt) > 0 && opt[0].OnAuthorizationHeaderInvalid != nil {
+				opt[0].OnAuthorizationHeaderInvalid(c)
+			} else {
+				c.AbortWithStatus(http.StatusBadRequest)
+			}
+			return
+		}
+
+		// Token value is invalid
+		if authTokens[1] != token {
+			if len(opt) > 0 && opt[0].OnTokenInvalid != nil {
+				opt[0].OnTokenInvalid(c)
+			} else {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+			return
+		}
+
+		// Everything looks fine, process next action
+		c.Next()
+	}
+}
